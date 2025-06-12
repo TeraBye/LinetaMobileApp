5b9894d7f8f1a26d6610a2d67a1842f9c6b42225
LDThien999 <leepro0611@gmail.com>
2025-06-12 15:30:08 +0700

    continue update

---
+++ app/src/main/java/com/example/lineta/dto/response/UnreadCountResponse.java
@@ -0,0 +1,13 @@
+package com.example.lineta.dto.response;
+
+public class UnreadCountResponse{
+    private long unreadCount;
+
+    public long getUnreadCount() {
+        return unreadCount;
+    }
+
+    public void setUnreadCount(long unreadCount) {
+        this.unreadCount = unreadCount;
+    }
+}
